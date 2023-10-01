687139acd54d97ecab29ff832d0d1ed9115fbfad
Vollate <uint44t@gmail.com>
2023-10-01 13:47:25 +0800

    add clang-format

--- include/Lexer.hpp
+++ include/Lexer.hpp
@@ -1,14 +1,18 @@
 #pragma once
 
 #include <string>
+
 #ifndef yyFlexLexerOnce
+
 #include <FlexLexer.h>
+
 #endif
 
 class Lexer : public yyFlexLexer {
-  std::string parseResult;
+    std::string parseResult;
 
 public:
-  virtual int yylex();
-  std::string lexicalAnalysis(const char *filePath);
+    virtual int yylex();
+
+    std::string lexicalAnalysis(const char *filePath);
 };
--- test/lexer/LexerTest.cpp
+++ test/lexer/LexerTest.cpp
@@ -1,15 +1,16 @@
-#include "Lexer.hpp"
 #include <stdexcept>
 #include <string>
 
-int main(int argc, char **argv) {
-  if (argc == 1) {
-    throw std::runtime_error("no argument");
-  }
-    Lexer instance;
-  for (int i = 1; i < argc; ++i) {
-    std::cout << instance.lexicalAnalysis(argv[i]) << std::endl;
-  }
+#include "Lexer.hpp"
 
-  return 0;
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        throw std::runtime_error("no argument");
+    }
+    Lexer instance;
+    for (int i = 1; i < argc; ++i) {
+        std::cout << instance.lexicalAnalysis(argv[i]) << std::endl;
+    }
+
+    return 0;
 }
