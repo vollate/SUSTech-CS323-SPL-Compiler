27d705a5a83ee696e68ce7a25046304390b1cced
Vollate <uint44t@gmail.com>
2023-10-28 16:18:47 +0800

    fix(no generate directory): force cmake create the pre_compile directory

--- src/Frontage.cpp
+++ src/Frontage.cpp
@@ -15,34 +15,35 @@ void Frontage::clear() {
     m_ast.clear();
     emptyParentStack();
 }
-static void recursiveConvert(std::stringstream& s, const ASTNode* node) {
-//TODO: add type checking @JYF
-   try {
+static void recursiveConvert(std::stringstream& s, const std::unique_ptr<ASTNode>& node) {
+    // TODO: add type checking @JYF
+    try {
         s << " " << std::get<int>(node->value);
-    }catch (std::bad_variant_access&) {
+    } catch(std::bad_variant_access&) {
         try {
             s << " " << std::get<float>(node->value);
-        }catch (std::bad_variant_access&) {
+        } catch(std::bad_variant_access&) {
             try {
                 s << " " << std::get<std::string>(node->value);
-            }catch (std::bad_variant_access&) {
-                s << " " << "error";
+            } catch(std::bad_variant_access&) {
+                s << " "
+                  << "error";
             }
         }
     }
-   for(auto& subNode : node->subNodes) {
-        recursiveConvert(s, subNode.get());
+    for(auto& subNode : node->subNodes) {
+        recursiveConvert(s, subNode);
     }
 }
-std::string Frontage::str() const {//TODO
+std::string Frontage::str() const {  // TODO
     std::stringstream s;
     for(auto& node : m_ast) {
-        recursiveConvert(s, node.get());
+        recursiveConvert(s, node);
     }
     return s.str();
 }
 
-void Frontage::increaseLocation(int32_t loc) {//TODO:修正位置信息@JYF
+void Frontage::increaseLocation(int32_t loc) {  // TODO:修正位置信息@JYF
     m_location += loc;
     cout << "increaseLocation(): " << loc << ", total = " << m_location << endl;
 }
